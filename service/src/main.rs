35a97b025e188b777aac8af202812fffbab5882a
hgonomeg <hgonomeg@gmail.com>
2022-09-24 12:55:45 +0200

    Setup actix

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{post, App, HttpResponse, HttpServer, Responder};
+
+
+#[post("/enqueue")]
+async fn enqueue(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(enqueue)
+    })
+    .bind(("127.0.0.1", 8090))?
+    .run()
+    .await
 }
