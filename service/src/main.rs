6fbd19e52b259723cc5c485e0b756cc46277fbef
hgonomeg <hgonomeg@gmail.com>
2022-09-24 14:05:55 +0200

    Make writing to the queue file operational

--- service/src/main.rs
+++ service/src/main.rs
@@ -4,10 +4,18 @@ use thiserror::Error;
 use std::{path::Path, borrow::Borrow};
 use anyhow::Result;
 use serde_json::from_str;
-use tokio::fs::OpenOptions;
+use tokio::{fs::OpenOptions,io::AsyncWriteExt};
 
-async fn append_to_file(a: &Path, data: &[u8]) {
+async fn append_to_file(a: &Path, data: &[u8]) -> Result<(),MyError> {
+    let mut file = OpenOptions::new()
+        .append(true)
+        .open(a).await?;
+    Ok(file.write_all(data).await?)
+}
 
+fn sanitize(youtube_url: String) -> Result<String,MyError> {
+    let sanitized = youtube_url;
+    Ok(sanitized)
 }
 
 #[derive(Debug,Deserialize)]
@@ -19,11 +27,13 @@ struct EnqueueRequest {
 #[derive(Debug,Error)]
 enum MyError {
     #[error("Failed to deserialize the JSON: {0}")]
-    DeserializationError(#[from] serde_json::Error)
+    DeserializationError(#[from] serde_json::Error),
+    #[error("Unable to perform I/O operation: {0}")]
+    IoOperationError(#[from] std::io::Error)
 }
 
 impl ResponseError for MyError {
-    //nothing
+    //nothing needed
 }
 
 
@@ -31,7 +41,10 @@ impl ResponseError for MyError {
 async fn enqueue(req_body: String) -> Result<String,MyError> {
     eprintln!("Received: {}",&req_body);
     let enqueue_request : EnqueueRequest = from_str(&req_body)?;
-    Ok(req_body)
+    let mut url = sanitize(enqueue_request.url)?;
+    url.push('\n');
+    append_to_file(std::path::Path::new("queue.txt"), url.as_bytes()).await?;
+    Ok(url)
 }
 
 
