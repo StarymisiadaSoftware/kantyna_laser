3cdf9588257df4249f5dcc3266988de1ecdb83e8
hgonomeg <hgonomeg@gmail.com>
2022-09-24 15:55:33 +0200

    GUWNO

--- website/src/main.rs
+++ website/src/main.rs
@@ -1,9 +1,49 @@
 use yew::prelude::*;
+use yew::TargetCast;
+struct Form {
+    url: String,
+}
+
+enum Msg {
+    InputValue(String),
+    Submit
+}
+
+impl Component for Form {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(ctx: &Context<Self>) -> Self {
+        Form{
+            url: String::default()
+        }
+    }
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let link = ctx.link();
+        let onclick = link.callback(|e: Event| {
+            Msg::Submit
+        });
+        html! {
+            <form>
+                <input type="text" id="twoj_stary" />
+                <button id="przechuj" onlick={onclick} >{"Pierdol sie"}</button>
+            </form>
+        }
+    }
+
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        false
+    }
+}
 
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <h1>{ "Hello World" }</h1>
+        <div class="chuj_mnie_strzeli">
+            <h1>{"Dodaj do kolejki"}</h1>
+            <h2>{"Lorem kurwa ipsum ci w dupe na śniadanie dolor sit amet jego mać"}</h2>
+            <Form/>
+        </div>
     }
 }
 
