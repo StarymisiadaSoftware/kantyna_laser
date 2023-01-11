9e9cb0082976e2613c8c186f8bc8c29e524f9c54
hgonomeg <hgonomeg@gmail.com>
2023-01-11 18:48:33 +0100

    Slightly improve code layout

--- website/src/lib.rs
+++ website/src/lib.rs
@@ -1,3 +1,4 @@
+use anyhow::Context;
 use common::EnqueueRequest;
 use js_sys::eval as js_eval;
 use seed::{prelude::*, *};
@@ -23,60 +24,58 @@ fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
     match msg {
         Msg::Submit => {
             log!("Inside Submit handler.");
-            let endpoint = js_eval(
-                r#"
-                    window.location.hostname
-                "#,
-            );
-            match endpoint {
-                Ok(endpoint_value) => {
-                    if let Some(endpoint) = endpoint_value.as_string() {
-                        log!("Got endpoint from JS: ", &endpoint);
-                        let endpoint = endpoint.replace("?", "");
-                        //let mut endpoint = endpoint.replace("8080", "8090");
-                        //endpoint.push_str(":8090/enqueue");
-                        let endpoint = format!("http://{}:8090/enqueue", endpoint);
-                        log!("Final endpoint: ", &endpoint);
-                        let post =
-                            Request::new(endpoint)
-                                .method(Method::Post)
-                                .json(&EnqueueRequest {
-                                    url: model.youtube_url.clone(),
-                                });
-                        match post {
-                            Ok(r) => {
-                                wasm_bindgen_futures::spawn_local(async move {
-                                    log!("Hello from POST-sending future.");
-                                    match r.fetch().await {
-                                        Ok(res) => {
-                                            log!("Got response: {:?}", res.text().await);
-                                        }
-                                        Err(e) => {
-                                            let e = format!("{:?}", e);
-                                            log!("Failed to send request: ", e);
-                                        }
-                                    }
-                                    log!("POST-sending future completes.");
-                                });
-                            }
-                            Err(e) => {
-                                let e = format!("{:?}", e);
-                                log!("Failed to create POST request: ", e);
-                            }
-                        }
-                    } else {
-                        log!("'window.location.host' could not be evaluated to a string.");
-                    }
-                }
-                Err(e) => {
-                    log!("Could not evaluate 'window.location.host'.", e);
-                }
-            }
+            run_submit(model.youtube_url.clone());
         }
         Msg::InputValue(url) => {
             log!("Handling URL change: ", &url);
             model.youtube_url = url;
         }
+    }
+}
+
+fn run_submit(url: String) {
+    let endpoint = js_eval(
+        r#"
+            window.location.hostname
+        "#,
+    );
+    let inner = move || {
+        let Ok(Some(endpoint)) = endpoint.map(|x| x.as_string()) else {
+            anyhow::bail!("'window.location.host' could not be evaluated to a string.");
+        };
+        log!("Got endpoint from JS: ", &endpoint);
+        let endpoint = endpoint.replace("?", "");
+        //let mut endpoint = endpoint.replace("8080", "8090");
+        //endpoint.push_str(":8090/enqueue");
+        let endpoint = format!("http://{}:8090/enqueue", endpoint);
+        log!("Final endpoint: ", &endpoint);
+        let post = Request::new(endpoint)
+            .method(Method::Post)
+            .json(&EnqueueRequest { url });
+        match post {
+            Ok(r) => {
+                wasm_bindgen_futures::spawn_local(async move {
+                    log!("Hello from POST-sending future.");
+                    match r.fetch().await {
+                        Ok(res) => {
+                            log!("Got response: {:?}", res.text().await);
+                        }
+                        Err(e) => {
+                            let e = format!("{:?}", e);
+                            log!("Failed to send request: ", e);
+                        }
+                    }
+                    log!("POST-sending future completes.");
+                });
+            }
+            Err(e) => {
+                anyhow::bail!("Failed to create POST request: {:?}", e)
+            }
+        }
+        anyhow::Ok(())
+    };
+    if let Err(e) = inner() {
+        log!(e);
     }
 }
 
